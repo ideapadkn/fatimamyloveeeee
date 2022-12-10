2324579cd03038af5eaadacb44b6f26f8a301a4b
Kamron or Камрон <kamrush6429@gmail.com>
2022-12-11 00:10:53 +0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
 window.onload = function() {
-
 	setTimeout(function() {
-
 		document.body.classList.add('loaded')
-
 		if (window.matchMedia('(min-width: 992px)').matches) { // If not mobile
-
 			Draggable.create('.gallery', {
 				bounds: 'body',
 				inertia: true
 			})
-			
 		}
-
 	}, 3000)
-
 }
